44ca4314c77d4851914d13e939246e26faa79c1f
Aaron Nolan <anolan23@yahoo.com>
2022-07-17 21:58:24 -0500

    add event

---
+++ components/Event/index.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+
+export interface Event {
+  image?: string;
+  title: string;
+  game: string;
+  summary: string;
+  attendees: number;
+  date: Date;
+}
+
+interface Props {
+  event: Event;
+}
+
+function EventComponent({ event }: Props) {
+  return (
+    <div className="event">
+      <div className="event__image">
+        <Image
+          src={event.image || '/event.jpeg'}
+          alt="event"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="event__content">
+        <div className="event__content__text">
+          <span className="event__title">{event.title}</span>
+          <span className="event__game">{event.game}</span>
+        </div>
+        <div className="event__content__bottom">
+          <div className="event__content__bottom__date">
+            <span className="material-icons">schedule</span>
+            <span className="event__content__bottom__date__val">
+              {event.date.toLocaleDateString()}
+            </span>
+          </div>
+          <div className="event__content__bottom__attendees">
+            <span className="material-icons">person</span>
+            <span className="event__content__bottom__attendees__count">
+              {event.attendees}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default EventComponent;
--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,12 +1,24 @@
 import Navbar from '../../components/Navbar';
 import Button from '../../components/Button';
+import Event from '../../components/Event';
 
 function Explore() {
   return (
-    <div>
+    <div className="explore">
       <Navbar>
         <Button color="primary">Create event</Button>
       </Navbar>
+      <div className="explore__content">
+        <Event
+          event={{
+            title: 'Halo LAN Party',
+            game: 'Halo 3',
+            summary: 'This is a summary',
+            attendees: 7,
+            date: new Date(),
+          }}
+        />
+      </div>
     </div>
   );
 }
